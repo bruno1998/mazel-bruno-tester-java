a7fac834a46b0f537e7fc7454527572f3aaaf1d1
bruno.mazel <bruno.mazel@adelya.com>
2023-10-14 06:04:39 +0200

    feat: if you stay less then half an hour it is free

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,6 +5,8 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
+    private double freeParkingLimitTimeInHour = 0.5;
+
     public void calculateFare(Ticket ticket) {
         if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
             throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
@@ -18,15 +20,31 @@ public class FareCalculatorService {
 
         switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                if (freeParkingUnderHalfAnHour(duration)) {
+                    ticket.setPrice(0);
+                } else {
+                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                }
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                if (freeParkingUnderHalfAnHour(duration)) {
+                    ticket.setPrice(0);
+                } else {
+                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                }
                 break;
             }
             default:
                 throw new IllegalArgumentException("Unkown Parking Type");
+        }
+    }
+
+    private boolean freeParkingUnderHalfAnHour(double duration) {
+        if (duration >= freeParkingLimitTimeInHour) {
+            return false;
+        } else {
+            return true;
         }
     }
 }
